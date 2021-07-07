243766b6d26131dd90d212d60390b893c3248e2e
Abhinav <sainiabhinav96@gmail.com>
2021-07-07 17:32:27 +0530

    Create Stickler Thief.cpp

---
+++ Dynamic Programming/Pattern_FibbonaciNumbers/Stickler Thief.cpp
@@ -0,0 +1,30 @@
+/*
+Problem : Stickler Thief
+Link : https://practice.geeksforgeeks.org/problems/stickler-theif-1587115621/1
+*/
+class Solution
+{
+    public:
+    //Function to find maximum Sum of Non adjacent Elements
+    int FindMaxSum(int arr[], int n)
+    {
+        int dp[n];
+        dp[0] = arr[0];
+        dp[1] = arr[1];
+        dp[2] = arr[0]+arr[2];
+        
+        for(int i=3;i<n;i++)
+            dp[i] = max(dp[i-2],dp[i-3])+arr[i];
+        
+        int max = 0;
+        for(int i=0;i<n;i++)
+            if(dp[i]>max)
+                max = dp[i];
+
+        return max;
+    }
+};
+/*
+Approach : Similar approach as Longest Increasing Subsequence, except that there is no need of second inner loop as per problem.
+Time - O(N), Space - O(N)
+*/

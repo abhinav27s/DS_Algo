af9e255b3d11f8c3e5ce323903d9f2ced478c8ea
Abhinav <sainiabhinav96@gmail.com>
2021-07-07 18:34:16 +0530

    Update

--- Dynamic Programming/Pattern_FibbonaciNumbers/House Robber.cpp
+++ Dynamic Programming/Pattern_FibbonaciNumbers/House Robber.cpp
@@ -1,17 +1,17 @@
 /*
-Problem : Stickler Thief
-Link : https://practice.geeksforgeeks.org/problems/stickler-theif-1587115621/1
+Problem : House Robber
+Link : https://leetcode.com/problems/house-robber/
 */
-class Solution
-{
-    public:
-    //Function to find maximum Sum of Non adjacent Elements
-    int FindMaxSum(int arr[], int n)
-    {
+class Solution {
+public:
+    int rob(vector<int>& arr) {
+        int n = arr.size();
         int dp[n];
         dp[0] = arr[0];
-        dp[1] = arr[1];
-        dp[2] = arr[0]+arr[2];
+        if(n>1)
+            dp[1] = arr[1];
+        if(n>2)
+            dp[2] = arr[0]+arr[2];
         
         for(int i=3;i<n;i++)
             dp[i] = max(dp[i-2],dp[i-3])+arr[i];

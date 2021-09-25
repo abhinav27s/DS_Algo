16b4700983f6f2244166e9240452860f82b17f07
Abhinav <sainiabhinav96@gmail.com>
2021-09-25 23:21:00 +0530

    Update LRUCache

--- Hashing/LRUCache.cpp
+++ Hashing/LRUCache.cpp
@@ -2,83 +2,91 @@
 Problem: LRU Cache
 Link: https://leetcode.com/problems/lru-cache/
 */
-class CacheList {
-public:
+class CacheList{
+    public:
     int key;
-    CacheList* next;
-}*head, * tail;
-
-int cacheSize, maxSize;
-map<int, int> hashMap;
+    CacheList *next, *prev;
+}*head, *tail;
 
 class LRUCache {
-    void updateCacheList(int key) {
-        if (head->key == key)
+    
+    unordered_map<int, pair<int, CacheList*> > hashMap;
+    int maxCapacity, curCapacity;
+    void updateCacheList(CacheList *p){
+        if(p==head || p==NULL)
             return;
-
-        CacheList* ptr = head, * back = NULL;
-        while (ptr) {
-            if (ptr->key == key && back) {
-                back->next = ptr->next;
-                if (ptr == tail)
-                    tail = back;
-                ptr->next = head;
-                head = ptr;
-                return;
-            }
-            back = ptr;
-            ptr = ptr->next;
-        }
+        p->prev->next = p->next;
+        
+        if(p->next)
+            p->next->prev = p->prev;
+        
+        if(p==tail)
+            tail = p->prev;
+        
+        p->next = head;
+        p->prev = NULL;
+        head->prev = p;
+        head = p;
     }
-    void addNewKey(int key, int value) {
-        if (cacheSize < maxSize) {
-            CacheList* node = new CacheList;
-            node->key = key;
+    void addCacheList(int key,int value){
+        CacheList *node = new CacheList();
+        node->key = key;
+        node->next = node->prev = NULL;
+        if(head==NULL)
+            head = tail = node;
+        else{
+            head->prev = node;
             node->next = head;
             head = node;
-            if (!tail)
-                tail = node;
-            cacheSize++;
         }
-        else {
-            int oldKey = tail->key;
-            hashMap.erase(oldKey);
-            tail->key = key;
+        hashMap[key].first = value;
+        hashMap[key].second = node;
+        if(curCapacity < maxCapacity)
+            curCapacity++;
+        else{
+            hashMap.erase(tail->key);
+            CacheList *tmp = tail->prev;
+            tmp->next = NULL;
+            delete(tail);
+            tail = tmp;
         }
-        hashMap[key] = value;
     }
 public:
     LRUCache(int capacity) {
-        cacheSize = 0;
-        maxSize = capacity;
-        CacheList* p = head, * tmp;
-        while (p) {
-            tmp = p->next;
-            delete(p);
-            p = tmp;
+        curCapacity = 0;
+        maxCapacity = capacity;
+        hashMap.clear();
+        CacheList *ptr = head;
+        while(ptr){
+            CacheList *tmp = ptr->next;
+            delete(ptr);
+            ptr = tmp;
         }
         head = tail = NULL;
-        hashMap.clear();
     }
-
+    
     int get(int key) {
-        if (hashMap.find(key) == hashMap.end())
+        if(hashMap.find(key)==hashMap.end())
             return -1;
-        updateCacheList(key);
-        return hashMap[key];
+        updateCacheList(hashMap[key].second);
+        return hashMap[key].first;
     }
-
+    
     void put(int key, int value) {
-
-        if (hashMap.find(key) == hashMap.end())
-            addNewKey(key, value);
+        if(hashMap.find(key)==hashMap.end())
+            addCacheList(key,value);
         else
-            hashMap[key] = value;
-
-        updateCacheList(key);
+            hashMap[key].first = value;
+        updateCacheList(hashMap[key].second);
     }
 };
 
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
 /*
 Approach - 2 data structures used - hashmap to store key,value pairs. (simple array or stl map, any can be used)
 and a linked list for LRU algorithm. If list capacity is full, then delete the last node and push new node while inserting new keys.
